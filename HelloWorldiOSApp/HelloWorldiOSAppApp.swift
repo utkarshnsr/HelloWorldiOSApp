0eb49f2522c12e1c606a52c564373d2dc0a5daa4
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2024-05-06 17:21:52 -0400

    Initial Commit

---
+++ HelloWorldiOSApp/HelloWorldiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldiOSAppApp.swift
+//  HelloWorldiOSApp
+//
+//  Created by Rajkumar Nattamai Subramanian on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
