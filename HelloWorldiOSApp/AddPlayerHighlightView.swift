e8d8b4d20185daa30a6a64f067c524cdf5cc6a70
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2024-05-08 11:45:45 -0400

    added navigation bar at bottom and tabview

---
+++ HelloWorldiOSApp/AddPlayerHighlightView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPlayerHighlightView.swift
+//  HelloWorldiOSApp
+//
+//  Created by Rajkumar Nattamai Subramanian on 5/8/24.
+//
+
+import SwiftUI
+
+struct AddPlayerHighlightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPlayerHighlightView()
+}
---
+++ HelloWorldiOSApp/MyPlayersView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPlayersView.swift
+//  HelloWorldiOSApp
+//
+//  Created by Rajkumar Nattamai Subramanian on 5/8/24.
+//
+
+import SwiftUI
+
+struct MyPlayersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPlayersView()
+}
